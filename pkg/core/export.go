dbb1fbe5838d44776d1c05bc79f7fd590042684d
LQing <linqing@ruijie.com.cn>
2024-08-07 18:50:21 +0800

    fix: init

--- pkg/core/export.go
+++ pkg/core/export.go
@@ -59,8 +59,23 @@ func printSecret(secrets []*Secret) {
 	w := tabwriter.NewWriter(os.Stdout, 10, 0, 5, ' ', tabwriter.TabIndent)
 	fmt.Fprintln(w, "UID\tKey\tValue")
 	for _, secret := range secrets {
+		firstLine := true
+
+		f := func(key, value string) {
+			if firstLine {
+				fmt.Fprintf(w, "%s\t%s\t%s\n", secret.UID, key, value)
+				firstLine = false
+				return
+			}
+			fmt.Fprintf(w, "%s\t%s\t%s\n", " ", key, value)
+		}
+
 		for k, v := range secret.Keys {
-			fmt.Fprintf(w, "%s\t%s\t%s\n", secret.UID, k, v)
+			f(k, v)
+		}
+
+		for _, file := range secret.Files {
+			f(file.Name, file.Description)
 		}
 	}
 	w.Flush()
